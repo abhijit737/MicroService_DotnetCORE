ed9db2d507a2539ab889f740cca801d8c83ac1a9
abhijit737 <abhijitm737@gmail.com>
2024-09-03 11:24:37 +0530

    ImplementedMicroservice

---
+++ Flex/CatalogService/Database/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace CatalogService.Database
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+    
+        public string Name { get; set; }
+    
+        public string Description { get; set; }
+
+        public Decimal UnitPrice { get; set; }
+    
+    }
+}
